94a4af02c36a6d23d8bebcb5302e9c46eca76d00
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-08 17:15:12 +0000

    Fix Task 0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,8 +11,8 @@ import re
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Return: the log message obfuscated """
-    return(re.sub(fr"({'|'.join(fields)})=.*?{separator}",
-           fr"\1={redaction}{separator}", message))
+    return(re.sub(rf"({'|'.join(fields)})=.*?{separator}",
+           rf"\1={redaction}{separator}", message))
 
 
 class RedactingFormatter(logging.Formatter):
