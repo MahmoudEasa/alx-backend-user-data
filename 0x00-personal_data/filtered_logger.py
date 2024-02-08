281f0bdb8e0f8a2f7bd71aeb946657c6ff0f5909
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-08 16:47:58 +0000

    Fix Task 0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,18 +12,3 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = re.compile(fr"({'|'.join(fields)})=.*?{separator}")
     replacement = fr"\g<1>={redaction}{separator}"
     return(re.sub(pattern, replacement, message))
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Format Function """
-        NotImplementedError
