a9451abe425e2a7ee7fbe00a6d5d66f4eb1c5a17
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-08 14:24:05 +0000

    Initial Files Personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+""" A function filter_datum that returns the log message obfuscated
+"""
+import logging
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Return: the log message obfuscated """
+    pass
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
