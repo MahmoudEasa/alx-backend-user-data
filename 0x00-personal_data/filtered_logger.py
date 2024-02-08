aba40aa9ac065c8218a5cb041d05b62ea9619c93
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-08 16:55:59 +0000

    Fix Task 0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+
 """ A function filter_datum that returns the log message obfuscated
 """
+
 import logging
 from typing import List
 import re
@@ -12,3 +14,18 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = re.compile(fr"({'|'.join(fields)})=.*?{separator}")
     replacement = fr"\g<1>={redaction}{separator}"
     return(re.sub(pattern, replacement, message))
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format Function """
+        NotImplementedError
