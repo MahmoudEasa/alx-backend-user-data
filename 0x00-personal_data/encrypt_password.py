fff4456243878c70517940ababe7db4a0e9e99d5
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-09 13:54:15 +0000

    Task 6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
     return (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validate that the provided password matches the hashed password """
+    return (bcrypt.checkpw(password.encode('utf-8'), hashed_password))
