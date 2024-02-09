bfe93c7ea1104e9868cd37d44a703bf07287ab31
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-09 07:17:47 +0000

    Fix pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ import re
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Return: the log message obfuscated """
-    return(re.sub(rf"({'|'.join(fields)})=.*?{separator}",
+    return (re.sub(rf"({'|'.join(fields)})=.*?{separator}",
            rf"\1={redaction}{separator}", message))
 
 
