1fad43e54d74983e8e5043b3f3e005124336f386
MahmoudEasa <MahmoudEasa@gmail.com>
2024-02-09 13:46:57 +0000

    Task 5. Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string """
+    return (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
