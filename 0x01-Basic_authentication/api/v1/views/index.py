001ce3de96d2df35266e0f9c8c12a1bb29799f6c
MahmoudEasa <mu01011422865@gmail.com>
2024-02-12 20:49:09 +0200

    Task 2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - raise a 401 error by using abort
     """
     return jsonify(abort(401))
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raise a 403 error by using abort
+    """
+    return jsonify(abort(403))
