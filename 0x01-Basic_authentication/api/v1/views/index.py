ee35ce20b538a4f6ca73b35bed905d8a24546c60
MahmoudEasa <mu01011422865@gmail.com>
2024-02-12 20:42:01 +0200

    Task 1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raise a 401 error by using abort
+    """
+    return jsonify(abort(401))
