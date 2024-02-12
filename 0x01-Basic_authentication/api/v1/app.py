2b861b3b9077e981eaee1909452e8414d1d9d552
MahmoudEasa <mu01011422865@gmail.com>
2024-02-12 22:25:57 +0200

    Task 5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if getenv("AUTH_TYPE", None):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    if auth:
+        paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+        if auth.require_auth(request.path, paths):
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from flask import request
 class Auth:
     """ Class to manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False - path and excluded_paths """
+        """ Returns False if path in excluded_paths else True """
         if not path or not excluded_paths or not len(excluded_paths):
             return (True)
 
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Returns None - request will be the Flask request object """
-        return (None)
+        if not request or 'Authorization' not in request.headers:
+            return (None)
+        return (request.headers['Authorization'])
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns None - request will be the Flask request object """
