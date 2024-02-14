05986708f8a8388311e6d86ff95fe71332032073
MahmoudEasa <mu01011422865@gmail.com>
2024-02-14 23:12:47 +0200

    Task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,15 @@ class SessionAuth(Auth):
         cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(cookie)
         return (User.get(user_id))
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return (False)
+
+        if not self.user_id_for_session_id(session_id):
+            return (False)
+
+        del self.user_id_by_session_id[session_id]
+        return (True)
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -16,21 +16,35 @@ def login() -> str:
     """
     email = request.form.get('email')
     if not email:
-        return jsonify({"error": "email missing"}), 400
+        return (jsonify({"error": "email missing"}), 400)
     password = request.form.get('password')
     if not password:
-        return jsonify({"error": "password missing"}), 400
+        return (jsonify({"error": "password missing"}), 400)
 
     user = User.search({"email": email})
     if not user:
-        return jsonify({"error": "no user found for this email"}), 404
+        return (jsonify({"error": "no user found for this email"}), 404)
 
     user = user[0]
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+        return (jsonify({"error": "wrong password"}), 401)
 
     from api.v1.app import auth
     session = auth.create_session(user.id)
     res = jsonify(user.to_json())
     res.set_cookie(getenv('SESSION_NAME'), session)
     return (res)
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ Deleting the Session ID contains in the request as cookie """
+    """ DELETE /auth_session/login
+    Return:
+        - Dictionary representation of the User
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return (jsonify({}), 200)
