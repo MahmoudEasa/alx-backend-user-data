2cbe960f16e02004a99e638a9400ba9f7e16dbf8
MahmoudEasa <mu01011422865@gmail.com>
2024-02-14 20:17:55 +0200

    Task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,9 +33,11 @@ def before_request():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/'
+            '/api/v1/auth_session/login/'
             ]
         if auth.require_auth(request.path, paths):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and\
+                    not auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
