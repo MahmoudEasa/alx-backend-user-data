c615959f42ac483b2cdd3eb98f29462b74fe3bf0
MahmoudEasa <mu01011422865@gmail.com>
2024-02-15 17:20:36 +0200

    Fix Task 9

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,17 +14,18 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv("AUTH_TYPE", None) == 'auth':
-    from api.v1.auth.auth import Auth
+auth_type = getenv("AUTH_TYPE", None)
+if auth_type == 'auth':
+    from .auth.auth import Auth
     auth = Auth()
-elif getenv("AUTH_TYPE", None) == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
+elif auth_type == 'basic_auth':
+    from .auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif getenv("AUTH_TYPE", None) == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
+elif auth_type == 'session_auth':
+    from .auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif getenv("AUTH_TYPE", None) == 'session_exp_auth':
-    from api.v1.auth.session_exp_auth import SessionExpAuth
+elif auth_type == 'session_exp_auth':
+    from .auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 
 
