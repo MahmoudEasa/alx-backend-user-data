9893ddabc66dbc0bb3f5de815e1ff6ab46ca9416
MahmoudEasa <mu01011422865@gmail.com>
2024-02-20 22:32:15 +0200

    Task 6. Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""App model
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """GET /
+        the home route
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
